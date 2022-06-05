c520325b2eb1713e7a4e4c05ea91ec4ce85647a2
Vinicius Fortunato <vinicius.fortunato@grupoboticario.com>
2022-06-05 18:39:47 -0300

    Add improvements to ModalNewEntry

--- src/modals/ModalNewEntry/ModalNewEntry.styles.ts
+++ src/modals/ModalNewEntry/ModalNewEntry.styles.ts
@@ -1,15 +1,17 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
 export const Wrapper = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
-  margin-top: 22px;
+  justify-content: end;
+  margin: 0px 10px;
 `;
 
 export const Container = styled.View`
-  background-color: white;
-  border-radius: 5px;
-  padding: 20px;
-  margin: 20px;
+  ${({ theme }) => css`
+    width: 100%;
+    background-color: ${theme.colors.white};
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+    padding: 20px;
+  `}
 `;
